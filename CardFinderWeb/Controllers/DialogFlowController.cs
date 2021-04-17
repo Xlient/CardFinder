85460b4d77f06327daa48d020757b3365478213d
Xlient <lakaleigh.harris@gmail.com>
2021-04-17 00:22:51 -0400

    fixed issue with project folder not being add to remote repo

---
+++ CardFinderWeb/Controllers/DialogFlowController.cs
@@ -0,0 +1,71 @@
+﻿using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardFinder.Controllers
+{
+   
+    [Route("api")]
+    [ApiController]
+    public class DialogFlowController : Controller
+    {
+        private readonly ILogger<DialogFlowController> _logger;
+        public DialogFlowController( ILogger<DialogFlowController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        private static readonly JsonParser jsonParser =
+        new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
+        [HttpPost]
+        public async Task<ContentResult> DialogAction()
+        {
+            // Read the request JSON asynchronously, as the Google.Protobuf library
+            // doesn't (yet) support asynchronous parsing.
+            string requestJson;
+            using (TextReader reader = new StreamReader(Request.Body))
+            {
+                requestJson = await reader.ReadToEndAsync();
+            }
+
+            try
+            {
+                WebhookRequest request = jsonParser.Parse<WebhookRequest>(requestJson);
+               OriginalDetectIntentRequest originalDetectIntent = request.OriginalDetectIntentRequest;
+                originalDetectIntent.Payload
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message, e.InnerException, e.Source);
+                
+            }
+          
+
+            
+            WebhookResponse response = new WebhookResponse
+            {
+                // ...
+            };
+
+           
+            string responseJson = response.ToString();
+            return Content(responseJson, "application/json");
+        }
+       
+        public IActionResult PostChipResponse(string data) 
+        {
+            // IntentsClient client = new IntentsClientBuilder { }.Build();
+
+            // client.GetIntentAsync(name);
+            _logger.LogInformation(data);
+            return Ok();
+        }
+    }
+}
---
+++ CardFinderWeb/wwwroot/js/cfDialogflow.js
@@ -0,0 +1,25 @@
+﻿
+
+
+
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('df-chip-clicked', function (event) {
+    chrome.debugger.attach(event.currentTarget, "1.2", function () {
+        chrome.debugger.sendCommand(event.currentTarget, "df-messenger.DfChipClickedEvent", arguments)
+    })
+    text = "test"
+    console.log( event.currentTarget + JSON.stringify(event));
+    $.ajax({
+        type: "POST",
+        url: 'https://localhost:44308/api',
+        data: { data: text },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            console.log(msg);
+        },
+        error: function (e) {
+            console.log(e,e.responseText);
+        }
+    });
+});
